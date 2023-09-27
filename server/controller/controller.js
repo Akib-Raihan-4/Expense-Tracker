ef380d4a433b05a1f5d88d29af682a1093a0063a
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-09-26 23:15:18 -0400

    create categories and mongoDb is connected

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,6 +1,18 @@
+const model = require('../models/model')
 
-function createCategories(req,res){
-    res.json("get request from controller")
+
+async function createCategories(req, res) {
+    try {
+        const Create = new model.Categories({
+            type: "Savings",
+            color: "#1F3B5C"
+        });
+
+        const savedCategory = await Create.save();
+        return res.json(savedCategory);
+    } catch (err) {
+        return res.status(400).json({ message: `Error while creating categories: ${err.message}` });
+    }
 }
 
 module.exports= {
--- server/server.js
+++ server/server.js
@@ -3,15 +3,25 @@ const app = express()
 const cors = require('cors')
 
 require('dotenv').config({path: './config.env'})
-const port = process.env.PORT || 5000
+const port =  5000
 
 //middleware
 app.use(cors())
 app.use(express.json())
 
+//mongodb connection
+const con = require('./db/connection')
+
 //using Routes
 app.use(require("./routes/route"))
 
-app.listen(port, ()=>{
-    console.log("server is running on port: ", port)
+
+con.then(db=>{
+    if(!db)return process.exit(1)
+    app.listen(port, ()=>{
+        console.log("server is running on port: ", port)
+    })
+}).catch(error =>{
+    console.log("Connection Failed")
 })
+
