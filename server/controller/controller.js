54d097cd5b4de1759994481e92cdc7d2f5f0c68b
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-09-27 15:46:35 -0400

    create Transaction and get categories controller is done

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -4,8 +4,8 @@ const model = require('../models/model')
 async function createCategories(req, res) {
     try {
         const Create = new model.Categories({
-            type: "Savings",
-            color: "#1F3B5C"
+            type: "Transaction",
+            color: "#1F3251"
         });
 
         const savedCategory = await Create.save();
@@ -15,6 +15,38 @@ async function createCategories(req, res) {
     }
 }
 
+async function getCategories(req,res){
+    let data = await model.Categories.find({})
+
+    let filter = await data.map(v=>Object.assign({}, {type:v.type, color:v.color}))
+    return res.json(filter)
+}
+
+async function createTransaction(req, res) {
+    try {
+      if (!req.body) {
+        return res.status(400).json({ error: "No Data Provided" });
+      }
+  
+      const { name, type, amount } = req.body;
+  
+      const transaction = new model.Transaction({
+        name,
+        type,
+        amount,
+        date: new Date(),
+      });
+  
+      await transaction.save();
+  
+      return res.status(201).json({ message: "Transaction created successfully" });
+    } catch (error) {
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  }
+
 module.exports= {
-    createCategories
+    createCategories,
+    getCategories,
+    createTransaction
 }
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,5 +1,9 @@
 const routes = require('express').Router()
 const controller = require("../controller/controller")
 routes.route('/api/categories').post(controller.createCategories)
+    .get(controller.getCategories)
+
+
+routes.route('/api/transaction').post(controller.createTransaction)
 
 module.exports = routes
